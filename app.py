e5397eb726425022256b3957c7cd4f89988939bc
Doug Campbell <ndouglascampbell@gmail.com>
2015-09-14 21:06:39 -0500

    moved html to template and added routing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 import os
 from werkzeug import secure_filename
 
@@ -9,29 +9,26 @@ app = Flask("Unbiased")
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
 
 
-@app.route("/", methods=['GET', 'POST'])
-def upload_file():
+@app.route("/<filename>", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'], defaults={'filename': None})
+def upload_file(filename):
     if request.method == 'POST':
         file = request.files['file']
         if file and check_extention(file.filename):
-            filename = secure_file(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_DIR'], filename))
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_DIR'], filename)
+            file.save(filepath)
             return redirect(url_for('upload_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-    
+    return render_template('upload.html', filename=filename)
 
 
 def check_extention(filename):
     if filename.rsplit('.', 1)[1] in ALLOWED_EXT:
-        return
+        return True
+    else:
+        print "%s does not contain approved extention" % filename
+        return False
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
